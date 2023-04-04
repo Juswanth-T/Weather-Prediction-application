245cf707cfebdcf4848c17e683c09ea484d88ec2
Juswanth-T <95039624+Juswanth-T@users.noreply.github.com>
2023-04-05 01:49:19 +0530

    Delete lnode.js

--- lnode.js
+++
@@ -1,9 +0,0 @@
-function location(){
-    fetch("https://api.ipdata.co/?api-key=1f4052c3649af43032ed28e4dd298128c35459e3acccf27dfcaaef41").then((response)=>{
-        response.json()
-    }).then((data)=>{
-        console.log(data.city)
-    }).catch((err)=>{
-        console.log(err)
-    })
-}
